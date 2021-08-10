0a449dbc6a2066417c9a11cfd568ab8b7c510a91
Peter Cho <80090757+HubbBuns@users.noreply.github.com>
2021-08-10 16:20:53 -0400

    Add files via upload

---
+++ percentage_of_sales.sql
@@ -0,0 +1,13 @@
+USE classicmodels;
+
+SELECT
+	p.productLine AS product_category,	#getting product lines
+    SUM(od.quantityOrdered * od.priceEach) AS total_sales	#taking the sum of quantity * price equaling to total sales
+FROM products p
+JOIN orderdetails od	
+	USING (productCode)
+JOIN orders o	#joining these tables to get the necessary columns
+	USING (orderNumber)
+WHERE status = "Shipped" OR status = "Resolved"	#to filter out orders where payment has been released
+GROUP BY product_category	#grouping by product lines, ie. Classic Car, Vintage Cars, Motorcycles, etc.
+ORDER BY total_sales DESC	#ordering products from greatest to least
---
+++ quarterly_year_sales.sql
@@ -0,0 +1,16 @@
+USE classicmodels;
+
+SELECT 
+	YEAR(paymentDate) AS year,	#getting the year
+    ROUND(AVG(amount), 0) AS total_sales, #getting the average amount of sales, rounded to a whole number
+    CASE 														#Using the case statement to get sales between certain months for each quarter, ie. Jan-Mar = Q1
+		WHEN MONTH(paymentDate) BETWEEN "01" AND "03" THEN "Q1"
+		WHEN MONTH(paymentDate) BETWEEN "04" AND "06" THEN "Q2"
+		WHEN MONTH(paymentDate) BETWEEN "07" AND "9" THEN "Q3"
+		ELSE "Q4"
+	END AS quarterly_sales
+FROM payments 
+GROUP BY year, quarterly_sales	#Grouping to get each quarter, in each year
+ORDER BY year 
+
+
